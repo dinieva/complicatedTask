41af46bf04901cf91adf16475842415fb865e31c
Rezeda <dinievarezeda@mail.ru>
2022-12-07 21:44:32 +0300

    добавила вывод в консоль

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let arr = ['1234', '4376', '245454', '476554', '87', '23', '45'];
 
 for (let elem of arr) {
-    if(elem[0] == 2 || elem[0] == 4){
+    if(elem[0] === 2 || elem[0] === 4){
         console.log(elem);
     }
 }
@@ -12,10 +12,10 @@ for (let i = 2; i <= n; i++) {
 
     for (let j = 2; j <= i; j++) {
 
-        if (i / j == 1) {
-            console.log(i);
+        if (i / j === 1) {
+            console.log(i +' - простое число.'+' Делители этого числа: 1 и '+  i);
             break;
-        } else if (i % j == 0) {
+        } else if (i % j === 0) {
             break;
         } else {
             continue;
